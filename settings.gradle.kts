d22491bb1d20a0ecdcb828a322917de70a17c4d2
Steve <61917452+awildergoose@users.noreply.github.com>
2025-10-24 21:01:32 +0300

    Remove Forge and NeoForge from settings.gradle.kts
    Removed Forge and NeoForge repositories from plugin management.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,6 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven {
-            name = "Forge"
-            url = uri("https://maven.minecraftforge.net/")
-            content {
-                includeGroupAndSubgroups("net.minecraftforge")
-            }
-        }
-        maven {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
             content {
@@ -21,15 +14,6 @@ pluginManagement {
             url = uri("https://maven.parchmentmc.org")
             content {
                 includeGroupAndSubgroups("org.parchmentmc")
-            }
-        }
-        maven {
-            name = "NeoForge"
-            url = uri("https://maven.neoforged.net/")
-            content {
-                includeGroupAndSubgroups("net.neoforged")
-                includeGroup("codechicken")
-                includeGroup("net.covers1624")
             }
         }
         maven {
@@ -50,5 +34,3 @@ rootProject.name = "geckolib"
 
 include("common")
 include("fabric")
-include("forge")
-include("neoforge")
